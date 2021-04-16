6466627b441b23266ba47a336e49780a5ebd3c46
chenglyes <chenglyes@163.com>
2021-04-16 14:35:20 +0800

    Remove unnecessary macro from "Config.h" and add comments.

--- server/include/Config.h
+++ server/include/Config.h
@@ -1,8 +1,15 @@
+/*
+ * @FilePath: /simtochat/server/include/Config.h
+ * @Author: CGL
+ * @Date: 2021-04-16 14:32:32
+ * @LastEditors: CGL
+ * @LastEditTime: 2021-04-16 14:33:55
+ * @Description: 
+ *  Define related configurations for server.
+ */
 #ifndef SIMTOCHAT_SERVER_INCLUDE_CONFIG_H
 #define SIMTOCHAT_SERVER_INCLUDE_CONFIG_H
 
 #define SERVER_PORT         8010
-
-#define EPOLL_SIZE          100
 
 #endif // !SIMTOCHAT_SERVER_INCLUDE_CONFIG_H
